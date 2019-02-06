87901b0b735578d20f33cb7cb5816c83a4f00f2e
Benjamin Dehli <benjamin@arkitektum.no>
2019-02-06 23:54:53 +0100

    Add app component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,82 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Home from './views/Home';
+import About from './views/About';
+import Contact from './views/Contact';
+
+import ViewTab from './partials/ViewTab';
+
+import style from './App.scss';
+
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.keyDownFunction = this.keyDownFunction.bind(this);
+    this.state = {
+      views: {
+        49: {
+          character: 1,
+          keyCode: 49,
+          component: Home,
+          name: 'home'
+        },
+        50: {
+          character: 2,
+          keyCode: 50,
+          component: About,
+          name: 'about'
+        },
+        51: {
+          character: 3,
+          keyCode: 51,
+          component: Contact,
+          name: 'contact'
+        }
+      },
+      activeView: {
+        character: 1,
+        keyCode: 49,
+        component: Home,
+        name: 'home'
+      } 
+    }
+  }
+  setActiveView(view){
+    this.setState({
+      activeView: view
+    });
+  };
+  keyDownFunction(event){
+    if (this.state.views[event.keyCode]){
+      this.setActiveView(this.state.views[event.keyCode])
+    }
+  }
+  componentDidMount(){
+    document.addEventListener("keydown", this.keyDownFunction, false);
+  }
+  componentWillUnmount(){
+    document.removeEventListener("keydown", this.keyDownFunction, false);
+  }
+  renderActiveView() {
+    const ActiveView = this.state.activeView.component;
+    return <ActiveView />;
+  }
+  renderViewTabs() {
+    const viewTabs = Object.keys(this.state.views).map(keyCode => {
+      let view = this.state.views[keyCode];
+      let active = view.keyCode === this.state.activeView.keyCode;
+      return <ViewTab onClick={() => this.setActiveView(view)} key={view.keyCode} view={view} active={active}/>;
+    });
+    return <div className={style.viewTabs}>{viewTabs}</div>
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      {this.renderActiveView()}
+      {this.renderViewTabs()}
       </div>
-    );
+      );
   }
 }
 
